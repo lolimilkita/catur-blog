4934e4f4e40c9b0c0cd9ae662f8543e4b97e87c3
lolimilkita <caturcno.cr@gmail.com>
2022-05-16 21:20:01 +0700

    Belajar Laravel 8 | 3

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Catur Saputro",
+        "email" => "caturcno.cr@gmail.com",
+        "image" => "LegoProfil.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });

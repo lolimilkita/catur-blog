9ac3f6e64807fc40a1dc6699327e09bfe6072fb4
lolimilkita <caturcno.cr@gmail.com>
2022-05-26 01:42:16 +0700

    13. Searcing & Pagination

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ Tutorial dari <a href="https://youtube.com/playlist?list=PLFIM0718LjIWiihbBIq-SW
 
 ##Perekembangan tutorial
 ###12. Update UI
+
+###13. Search & Pagination
+Disini belajar membuat query search yang komplek, jadi di web ada pencarian post yang ada di kategory yang dipilih karena kita memiliki halaman post yang isinya category tersebut. Dan pagination di laravel mudah. fitur yang digunakan QueryScope
+
+<img src="https://github.com/lolimilkita/coba-laravel/blob/master/img_readme/dua_1.png">
+<img src="https://github.com/lolimilkita/coba-laravel/blob/master/img_readme/dua_2.png">
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -19,15 +19,13 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home",
-        "active" => "home"
+        "title" => "Home"
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
-        "active" => "about",
         "name" => "Catur Saputro",
         "email" => "catur.ccd@gmail.com",
         "image" => "LegoProfil.jpg"
@@ -41,22 +39,20 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function () {
     return view('categories', [
         'title' => 'Post Categories',
-        'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
 
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'title' => "Post By Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
+// Route::get('/categories/{category:slug}', function (Category $category) {
+//     return view('posts', [
+//         'title' => "Post By Category : $category->name",
+//         'posts' => $category->posts->load('category', 'author')
+//     ]);
+// });
 
-Route::get('/authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'title' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category', 'author')
-    ]);
-});
+// Route::get('/authors/{author:username}', function (User $author) {
+//     return view('posts', [
+//         'title' => "Post By Author : $author->name",
+//         'posts' => $author->posts->load('category', 'author')
+//     ]);
+// });

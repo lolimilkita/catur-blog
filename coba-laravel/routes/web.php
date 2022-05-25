8ec203c41126183d8df852d56605a0d830053f2c
lolimilkita <caturcno.cr@gmail.com>
2022-05-25 21:11:51 +0700

    Update Readme.md

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -19,13 +19,15 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => "home"
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" => "about",
         "name" => "Catur Saputro",
         "email" => "catur.ccd@gmail.com",
         "image" => "LegoProfil.jpg"
